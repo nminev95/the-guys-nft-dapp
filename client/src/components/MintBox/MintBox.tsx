ed8d7988710e2ede513031b371a0fa2fd1c2da1c
nminev95 <nminev95@gmail.com>
2023-05-07 22:41:38 +0300

    add base EIP-712 domain/struct/data to UI

--- client/src/components/MintBox/MintBox.tsx
+++ client/src/components/MintBox/MintBox.tsx
@@ -1,10 +1,48 @@
 import { Box } from '@chakra-ui/react'
 import Button from '@components/common/Button/Button'
+import { useEthers } from '@providers/EthersProvider/EthersProvider'
+import { ethers, verifyMessage } from 'ethers'
 
-const height = 450
-const width = 350
+const height = 560
+const width = 420
 
 const MintBox = () => {
+  const { signer } = useEthers()
+
+  const handleMintButtonClick = async () => {
+    const domain = {
+      name: 'TheGuysNFT',
+      version: '1.0.0',
+      chainId: 1,
+      verifyingContract: '0x1111111111111111111111111111111111111111'
+    }
+
+    const types = {
+      transaction: [
+        { name: 'from', type: 'Person' },
+        { name: 'to', type: 'Person' },
+        { name: 'content', type: 'string' }
+      ],
+      Person: [
+        { name: 'name', type: 'string' },
+        { name: 'wallet', type: 'address' }
+      ]
+    }
+
+    const mail = {
+      from: {
+        name: 'Alice',
+        wallet: '0x2111111111111111111111111111111111111111'
+      },
+      to: {
+        name: 'Bob',
+        wallet: '0x3111111111111111111111111111111111111111'
+      },
+      content: 'Hello!'
+    }
+    const signature = await signer?.signTypedData(domain, types, mail)
+    console.log(signature)
+  }
   return (
     <Box
       borderRadius="3xl"
@@ -21,7 +59,7 @@ const MintBox = () => {
       h={height}
       w={width}
     >
-      <Button>MINT NOW</Button>
+      <Button onClick={handleMintButtonClick}>MINT NOW</Button>
     </Box>
   )
 }
